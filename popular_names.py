200208c80c8c66f54e3788ad0e820b4af652c970
Nick Pack <nick@nickpack.com>
2015-07-09 12:55:44 +0100

    Enforce PEP8

--- popular_names.py
+++ popular_names.py
@@ -5,6 +5,7 @@ import sys
 import urllib
 import urllib2
 from HTMLParser import HTMLParser
+from functools import reduce
 
 
 __author__ = 'Nick Pack <nick@nickpack.com>'
@@ -15,6 +16,7 @@ data_table = []
 
 
 class UglyDataParser(HTMLParser):
+
     def __init__(self):
         HTMLParser.__init__(self)
         self.in_td = False
@@ -27,7 +29,8 @@ class UglyDataParser(HTMLParser):
         if self.in_td:
             if data.isdigit():
                 # This is quick and dirty, from my experiments I haven't hit a case where there are numbers in a td
-                # that aren't part of the data set, though this is likely brittle if they change something!
+                # that aren't part of the data set, though this is likely
+                # brittle if they change something!
                 data_table.append(data)
 
     def handle_endtag(self, tag):
@@ -49,13 +52,13 @@ def make_request(values):
 
     try:
         return response.read()
-    except urllib2.HTTPError, e:
+    except urllib2.HTTPError as e:
         print 'HTTP Error - %s ' % str(e.code)
         sys.exit(1)
-    except urllib2.URLError, e:
+    except urllib2.URLError as e:
         print 'URL Error - %s ' % str(e.reason)
         sys.exit(2)
-    except httplib.HTTPException, e:
+    except httplib.HTTPException as e:
         print 'HTTP Error'
         sys.exit(3)
     except Exception:
@@ -80,7 +83,8 @@ def parse_body(body, name, end_year):
     parser.feed(body)
     yearly_counts = []
 
-    # Index the data into a sensible format we can iterate and ignore as appropriate
+    # Index the data into a sensible format we can iterate and ignore as
+    # appropriate
     for i in range(len(data_table)):
         if i % 2 == 0 and len(data_table[i]) > 3 and data_table[i + 1]:
             data_dict[data_table[i]] = data_table[i + 1]
@@ -90,7 +94,8 @@ def parse_body(body, name, end_year):
             yearly_counts.append(int(value))
 
     if not yearly_counts:
-        # Although subjectively no data is an error, in this instance the script executed successfully but returned null
+        # Although subjectively no data is an error, in this instance the
+        # script executed successfully but returned null
         print 'No data available for %s in the specified date range' % name
         sys.exit(0)
 
@@ -110,10 +115,27 @@ def main():
                     '1000 results over a given period of time.'
     )
 
-    parser.add_argument('name', metavar='name', type=str, help='The name to look up')
-    # Technically this should validate >= 1900, but as the web service doesnt care, neither do I!
-    parser.add_argument('start', metavar='start_year', type=int, nargs='?', default=1900, help='The start year')
-    parser.add_argument('end', metavar='end_year', type=int, nargs='?', default=2014, help='The end year')
+    parser.add_argument(
+        'name',
+        metavar='name',
+        type=str,
+        help='The name to look up')
+    # Technically this should validate >= 1900, but as the web service doesnt
+    # care, neither do I!
+    parser.add_argument(
+        'start',
+        metavar='start_year',
+        type=int,
+        nargs='?',
+        default=1900,
+        help='The start year')
+    parser.add_argument(
+        'end',
+        metavar='end_year',
+        type=int,
+        nargs='?',
+        default=2014,
+        help='The end year')
     args = vars(parser.parse_args())
     # We're only interested in male names
     args['sex'] = 'M'
